ba11c5d855c5397b712ed992efe55f66aec30bab
jonmacphers <jonmacphers@gmail.com>
2022-03-21 09:45:50 -0400

    update

--- contracts/MasterGamer.sol
+++ contracts/MasterGamer.sol
@@ -1,8 +1,8 @@
 pragma solidity 0.6.12;
 
 import '@nextechlabs/nexdex-lib/contracts/math/SafeMath.sol';
-import '@nextechlabs/nexdex-lib/contracts/token/BEP20/IBEP20.sol';
-import '@nextechlabs/nexdex-lib/contracts/token/BEP20/SafeBEP20.sol';
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import '@nextechlabs/nexdex-lib/contracts/access/Ownable.sol';
 
 import "./Xp.sol";
@@ -11,41 +11,41 @@ import "./BoostBar.sol";
 // import "@nomiclabs/buidler/console.sol";
 
 interface IMigratorGamer {
-  // Perform LP token migration from legacy PancakeSwap to CakeSwap.
+  // Perform LP token migration from legacy PanxpSwap to XpSwap.
   // Take the current LP token address and return the new LP token address.
   // Migrator should have full access to the caller's LP token.
   // Return the new LP token address.
   //
-  // XXX Migrator must have allowance access to PancakeSwap LP tokens.
-  // CakeSwap must mint EXACTLY the same amount of CakeSwap LP tokens or
-  // else something bad will happen. Traditional PancakeSwap does not
+  // XXX Migrator must have allowance access to PanxpSwap LP tokens.
+  // XpSwap must mint EXACTLY the same amount of XpSwap LP tokens or
+  // else something bad will happen. Traditional PanxpSwap does not
   // do that so be careful!
-  function migrate(IBEP20 token) external returns (IBEP20);
+  function migrate(IERC20 token) external returns (IERC20);
 }
 
-// MasterGamer is the master of Cake. He can make Cake and he is a fair guy.
+// MasterGamer is the master of Xp. He can make Xp and he is a fair guy.
 //
 // Note that it's ownable and the owner wields tremendous power. The ownership
-// will be transferred to a governance smart contract once CAKE is sufficiently
+// will be transferred to a governance smart contract once XP is sufficiently
 // distributed and the community can show to govern itself.
 //
 // Have fun reading it. Hopefully it's bug-free. God bless.
 contract MasterGamer is Ownable {
   using SafeMath for uint256;
-  using SafeBEP20 for IBEP20;
+  using SafeERC20 for IERC20;
 
   // Info of each user.
   struct UserInfo {
     uint256 amount;     // How many LP tokens the user has provided.
     uint256 rewardDebt; // Reward debt. See explanation below.
     //
-    // We do some fancy math here. Basically, any point in time, the amount of CAKEs
+    // We do some fancy math here. Basically, any point in time, the amount of XPs
     // entitled to a user but is pending to be distributed is:
     //
-    //   pending reward = (user.amount * pool.accCakePerShare) - user.rewardDebt
+    //   pending reward = (user.amount * pool.accXpPerShare) - user.rewardDebt
     //
     // Whenever a user deposits or withdraws LP tokens to a pool. Here's what happens:
-    //   1. The pool's `accCakePerShare` (and `lastRewardBlock`) gets updated.
+    //   1. The pool's `accXpPerShare` (and `lastRewardBlock`) gets updated.
     //   2. User receives the pending reward sent to his/her address.
     //   3. User's `amount` gets updated.
     //   4. User's `rewardDebt` gets updated.
@@ -53,21 +53,21 @@ contract MasterGamer is Ownable {
 
   // Info of each pool.
   struct PoolInfo {
-    IBEP20 lpToken;           // Address of LP token contract.
-    uint256 allocPoint;       // How many allocation points assigned to this pool. CAKEs to distribute per block.
-    uint256 lastRewardBlock;  // Last block number that CAKEs distribution occurs.
-    uint256 accCakePerShare; // Accumulated CAKEs per share, times 1e12. See below.
+    IERC20 lpToken;           // Address of LP token contract.
+    uint256 allocPoint;       // How many allocation points assigned to this pool. XPs to distribute per block.
+    uint256 lastRewardBlock;  // Last block number that XPs distribution occurs.
+    uint256 accXpPerShare; // Accumulated XPs per share, times 1e12. See below.
   }
 
-  // The CAKE TOKEN!
-  Xp public cake;
+  // The XP TOKEN!
+  Xp public xp;
   // The SYRUP TOKEN!
-  BoostBar public syrup;
+  BoostBar public boost;
   // Dev address.
   address public devaddr;
-  // CAKE tokens created per block.
-  uint256 public cakePerBlock;
-  // Bonus muliplier for early cake makers.
+  // XP tokens created per block.
+  uint256 public xpPerBlock;
+  // Bonus muliplier for early xp makers.
   uint256 public BONUS_MULTIPLIER = 1;
   // The migrator contract. It has a lot of power. Can only be set through governance (owner).
   IMigratorGamer public migrator;
@@ -78,7 +78,7 @@ contract MasterGamer is Ownable {
   mapping (uint256 => mapping (address => UserInfo)) public userInfo;
   // Total allocation points. Must be the sum of all allocation points in all pools.
   uint256 public totalAllocPoint = 0;
-  // The block number when CAKE mining starts.
+  // The block number when XP mining starts.
   uint256 public startBlock;
 
   event Deposit(address indexed user, uint256 indexed pid, uint256 amount);
@@ -86,24 +86,24 @@ contract MasterGamer is Ownable {
   event EmergencyWithdraw(address indexed user, uint256 indexed pid, uint256 amount);
 
   constructor(
-    Xp _cake,
-    BoostBar _syrup,
+    Xp _xp,
+    BoostBar _boost,
     address _devaddr,
-    uint256 _cakePerBlock,
+    uint256 _xpPerBlock,
     uint256 _startBlock
     ) public {
-      cake = _cake;
-      syrup = _syrup;
+      xp = _xp;
+      boost = _boost;
       devaddr = _devaddr;
-      cakePerBlock = _cakePerBlock;
+      xpPerBlock = _xpPerBlock;
       startBlock = _startBlock;
 
       // staking pool
       poolInfo.push(PoolInfo({
-        lpToken: _cake,
+        lpToken: _xp,
         allocPoint: 1000,
         lastRewardBlock: startBlock,
-        accCakePerShare: 0
+        accXpPerShare: 0
         }));
 
         totalAllocPoint = 1000;
@@ -120,7 +120,7 @@ contract MasterGamer is Ownable {
 
       // Add a new lp to the pool. Can only be called by the owner.
       // XXX DO NOT add the same LP token more than once. Rewards will be messed up if you do.
-      function add(uint256 _allocPoint, IBEP20 _lpToken, bool _withUpdate) public onlyOwner {
+      function add(uint256 _allocPoint, IERC20 _lpToken, bool _withUpdate) public onlyOwner {
         if (_withUpdate) {
           massUpdatePools();
         }
@@ -130,12 +130,12 @@ contract MasterGamer is Ownable {
           lpToken: _lpToken,
           allocPoint: _allocPoint,
           lastRewardBlock: lastRewardBlock,
-          accCakePerShare: 0
+          accXpPerShare: 0
           }));
           updateStakingPool();
         }
 
-        // Update the given pool's CAKE allocation point. Can only be called by the owner.
+        // Update the given pool's XP allocation point. Can only be called by the owner.
         function set(uint256 _pid, uint256 _allocPoint, bool _withUpdate) public onlyOwner {
           if (_withUpdate) {
             massUpdatePools();
@@ -170,10 +170,10 @@ contract MasterGamer is Ownable {
         function migrate(uint256 _pid) public {
           require(address(migrator) != address(0), "migrate: no migrator");
           PoolInfo storage pool = poolInfo[_pid];
-          IBEP20 lpToken = pool.lpToken;
+          IERC20 lpToken = pool.lpToken;
           uint256 bal = lpToken.balanceOf(address(this));
           lpToken.safeApprove(address(migrator), bal);
-          IBEP20 newLpToken = migrator.migrate(lpToken);
+          IERC20 newLpToken = migrator.migrate(lpToken);
           require(bal == newLpToken.balanceOf(address(this)), "migrate: bad");
           pool.lpToken = newLpToken;
         }
@@ -183,18 +183,18 @@ contract MasterGamer is Ownable {
           return _to.sub(_from).mul(BONUS_MULTIPLIER);
         }
 
-        // View function to see pending CAKEs on frontend.
-        function pendingCake(uint256 _pid, address _user) external view returns (uint256) {
+        // View function to see pending XPs on frontend.
+        function pendingXp(uint256 _pid, address _user) external view returns (uint256) {
           PoolInfo storage pool = poolInfo[_pid];
           UserInfo storage user = userInfo[_pid][_user];
-          uint256 accCakePerShare = pool.accCakePerShare;
+          uint256 accXpPerShare = pool.accXpPerShare;
           uint256 lpSupply = pool.lpToken.balanceOf(address(this));
           if (block.number > pool.lastRewardBlock && lpSupply != 0) {
             uint256 multiplier = getMultiplier(pool.lastRewardBlock, block.number);
-            uint256 cakeReward = multiplier.mul(cakePerBlock).mul(pool.allocPoint).div(totalAllocPoint);
-            accCakePerShare = accCakePerShare.add(cakeReward.mul(1e12).div(lpSupply));
+            uint256 xpReward = multiplier.mul(xpPerBlock).mul(pool.allocPoint).div(totalAllocPoint);
+            accXpPerShare = accXpPerShare.add(xpReward.mul(1e12).div(lpSupply));
           }
-          return user.amount.mul(accCakePerShare).div(1e12).sub(user.rewardDebt);
+          return user.amount.mul(accXpPerShare).div(1e12).sub(user.rewardDebt);
         }
 
         // Update reward variables for all pools. Be careful of gas spending!
@@ -218,94 +218,94 @@ contract MasterGamer is Ownable {
             return;
           }
           uint256 multiplier = getMultiplier(pool.lastRewardBlock, block.number);
-          uint256 cakeReward = multiplier.mul(cakePerBlock).mul(pool.allocPoint).div(totalAllocPoint);
-          cake.mint(devaddr, cakeReward.div(10));
-          cake.mint(address(syrup), cakeReward);
-          pool.accCakePerShare = pool.accCakePerShare.add(cakeReward.mul(1e12).div(lpSupply));
+          uint256 xpReward = multiplier.mul(xpPerBlock).mul(pool.allocPoint).div(totalAllocPoint);
+          xp.mint(devaddr, xpReward.div(10));
+          xp.mint(address(boost), xpReward);
+          pool.accXpPerShare = pool.accXpPerShare.add(xpReward.mul(1e12).div(lpSupply));
           pool.lastRewardBlock = block.number;
         }
 
-        // Deposit LP tokens to MasterGamer for CAKE allocation.
+        // Deposit LP tokens to MasterGamer for XP allocation.
         function deposit(uint256 _pid, uint256 _amount) public {
 
-          require (_pid != 0, 'deposit CAKE by staking');
+          require (_pid != 0, 'deposit XP by staking');
 
           PoolInfo storage pool = poolInfo[_pid];
           UserInfo storage user = userInfo[_pid][msg.sender];
           updatePool(_pid);
           if (user.amount > 0) {
-            uint256 pending = user.amount.mul(pool.accCakePerShare).div(1e12).sub(user.rewardDebt);
+            uint256 pending = user.amount.mul(pool.accXpPerShare).div(1e12).sub(user.rewardDebt);
             if(pending > 0) {
-              safeCakeTransfer(msg.sender, pending);
+              safeXpTransfer(msg.sender, pending);
             }
           }
           if (_amount > 0) {
             pool.lpToken.safeTransferFrom(address(msg.sender), address(this), _amount);
             user.amount = user.amount.add(_amount);
           }
-          user.rewardDebt = user.amount.mul(pool.accCakePerShare).div(1e12);
+          user.rewardDebt = user.amount.mul(pool.accXpPerShare).div(1e12);
           emit Deposit(msg.sender, _pid, _amount);
         }
 
         // Withdraw LP tokens from MasterGamer.
         function withdraw(uint256 _pid, uint256 _amount) public {
 
-          require (_pid != 0, 'withdraw CAKE by unstaking');
+          require (_pid != 0, 'withdraw XP by unstaking');
           PoolInfo storage pool = poolInfo[_pid];
           UserInfo storage user = userInfo[_pid][msg.sender];
           require(user.amount >= _amount, "withdraw: not good");
 
           updatePool(_pid);
-          uint256 pending = user.amount.mul(pool.accCakePerShare).div(1e12).sub(user.rewardDebt);
+          uint256 pending = user.amount.mul(pool.accXpPerShare).div(1e12).sub(user.rewardDebt);
           if(pending > 0) {
-            safeCakeTransfer(msg.sender, pending);
+            safeXpTransfer(msg.sender, pending);
           }
           if(_amount > 0) {
             user.amount = user.amount.sub(_amount);
             pool.lpToken.safeTransfer(address(msg.sender), _amount);
           }
-          user.rewardDebt = user.amount.mul(pool.accCakePerShare).div(1e12);
+          user.rewardDebt = user.amount.mul(pool.accXpPerShare).div(1e12);
           emit Withdraw(msg.sender, _pid, _amount);
         }
 
-        // Stake CAKE tokens to MasterGamer
+        // Stake XP tokens to MasterGamer
         function enterStaking(uint256 _amount) public {
           PoolInfo storage pool = poolInfo[0];
           UserInfo storage user = userInfo[0][msg.sender];
           updatePool(0);
           if (user.amount > 0) {
-            uint256 pending = user.amount.mul(pool.accCakePerShare).div(1e12).sub(user.rewardDebt);
+            uint256 pending = user.amount.mul(pool.accXpPerShare).div(1e12).sub(user.rewardDebt);
             if(pending > 0) {
-              safeCakeTransfer(msg.sender, pending);
+              safeXpTransfer(msg.sender, pending);
             }
           }
           if(_amount > 0) {
             pool.lpToken.safeTransferFrom(address(msg.sender), address(this), _amount);
             user.amount = user.amount.add(_amount);
           }
-          user.rewardDebt = user.amount.mul(pool.accCakePerShare).div(1e12);
+          user.rewardDebt = user.amount.mul(pool.accXpPerShare).div(1e12);
 
-          syrup.mint(msg.sender, _amount);
+          boost.mint(msg.sender, _amount);
           emit Deposit(msg.sender, 0, _amount);
         }
 
-        // Withdraw CAKE tokens from STAKING.
+        // Withdraw XP tokens from STAKING.
         function leaveStaking(uint256 _amount) public {
           PoolInfo storage pool = poolInfo[0];
           UserInfo storage user = userInfo[0][msg.sender];
           require(user.amount >= _amount, "withdraw: not good");
           updatePool(0);
-          uint256 pending = user.amount.mul(pool.accCakePerShare).div(1e12).sub(user.rewardDebt);
+          uint256 pending = user.amount.mul(pool.accXpPerShare).div(1e12).sub(user.rewardDebt);
           if(pending > 0) {
-            safeCakeTransfer(msg.sender, pending);
+            safeXpTransfer(msg.sender, pending);
           }
           if(_amount > 0) {
             user.amount = user.amount.sub(_amount);
             pool.lpToken.safeTransfer(address(msg.sender), _amount);
           }
-          user.rewardDebt = user.amount.mul(pool.accCakePerShare).div(1e12);
+          user.rewardDebt = user.amount.mul(pool.accXpPerShare).div(1e12);
 
-          syrup.burn(msg.sender, _amount);
+          boost.burn(msg.sender, _amount);
           emit Withdraw(msg.sender, 0, _amount);
         }
 
@@ -319,9 +319,9 @@ contract MasterGamer is Ownable {
           user.rewardDebt = 0;
         }
 
-        // Safe cake transfer function, just in case if rounding error causes pool to not have enough CAKEs.
-        function safeCakeTransfer(address _to, uint256 _amount) internal {
-          syrup.safeCakeTransfer(_to, _amount);
+        // Safe xp transfer function, just in case if rounding error causes pool to not have enough XPs.
+        function safeXpTransfer(address _to, uint256 _amount) internal {
+          boost.safeXpTransfer(_to, _amount);
         }
 
         // Update dev address by the previous dev.
